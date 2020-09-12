8694508cea189d81da867f3ab733939e531e4916
Graeme Lockley <graemel@no9.co.za>
2020-09-12 15:21:29 +0200

    feature: upgrade console-cli

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import * as PP from "https://raw.githubusercontent.com/littlelanguages/deno-lib-text-prettyprint/0.2.2/mod.ts";
+import * as PP from "https://raw.githubusercontent.com/littlelanguages/deno-lib-text-prettyprint/0.3.0/mod.ts";
 
 export type Definition = {
   name: string;
@@ -212,6 +212,7 @@ function show(cli: Definition): Promise<void> {
           ),
         ],
       ),
+      "",
     ]),
     Deno.stdout,
   );
